83ddcef8e185de6543c67422a629b2ab3a3db5ca
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-12 22:27:14 +0200

    Reverse

--- Project1/src/Folder/OpenFolder.java
+++ Project1/src/Folder/OpenFolder.java
@@ -1,16 +1,5 @@
 package Folder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class OpenFolder {
-	String	destination;
-	String	path;
-	ArrayList<List<String>> 	matrix;
-	
-	public OpenFolder(String userDestination, String userPath){
-		destination = userDestination;
-		path = userPath;
-	}
-	
+
 }
