e2cfe75136b1190eab81b498217cd879f9dffbb2
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-13 14:05:47 +0200

    Update OpenFolder

--- Project1/src/Folder/OpenFolder.java
+++ Project1/src/Folder/OpenFolder.java
@@ -1,5 +1,53 @@
 package Folder;
 
-public class OpenFolder {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+import File.OpenFile;
+import Row.All_Rows;
+
+public class OpenFolder {
+	
+	String Location;
+	String Destination;
+	String File_Path;
+	OpenFile Open_File_Object;
+	All_Rows All_Rows_Object;
+	ArrayList<List<String>> new_Matrix, temp_Matrix;
+	
+	//Constructor
+	public OpenFolder(String userLocation, String userDestination){
+		this.Location = userDestination;
+		this.Destination = userDestination;
+	
+		openFolder(Location, Destination);
+	}
+	
+	//Generate a matrix with all CSV files
+	public ArrayList<List<String>> openFolder(String location, String destination) {
+		this.new_Matrix = new ArrayList<>();
+		this.temp_Matrix = new ArrayList<>();
+		
+		File folder = new File(location);
+		File[] listOfFiles = folder.listFiles();
+		for(int i = 0; i < listOfFiles.length; i++){
+			String filename = listOfFiles[i].getName();
+			if(filename.endsWith(".csv")){
+				this.File_Path = location+"/"+filename;
+				this.temp_Matrix = Open_File_Object.openFiles(File_Path);
+			}
+			updateMatrix(new_Matrix, temp_Matrix);
+		}		
+		All_Rows_Object = new All_Rows(new_Matrix);
+		
+		return new_Matrix;
+	}
+	
+	//Update new_Matrix all time 
+	public void updateMatrix(ArrayList<List<String>> new_Matrix, ArrayList<List<String>> temp_Matrix){
+		for (int i = 0; i < temp_Matrix.size(); i++) {
+			new_Matrix.add(temp_Matrix.get(i));
+		}
+	}
 }
