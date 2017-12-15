41c9704821401901939aa9cc0b2cfd49ef929e2c
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-15 13:30:36 +0200

    JAVADOC Edited

--- Project1/src/Run/Main.java
+++ Project1/src/Run/Main.java
@@ -1,12 +1,16 @@
 package Run;
 
 public class Main {
-
+	/**
+	 * Ariel University, OOP Course 
+	 * Course's Mission
+	 * @author חני בקבני, איתי בן משה, אביטל ישראלי
+	 */
 	public static void main(String[] args) {
-		
-		String userLocation = "C:/OOP/CSV";
+
+		String userLocation = "C:/OOP/CSV/";
 		String userDestination = "C:/OOP/CSV/OUT/";
-		
+
 		Run Go = new Run();
 		Go.Program(userLocation, userDestination);
 	}
