bed203847804580cabac87d32f683ff11eaa592d
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-17 23:12:28 +0200

    Bug Fixed

--- Project1/src/Filter/IDFilter.java
+++ Project1/src/Filter/IDFilter.java
@@ -5,31 +5,31 @@ import java.util.List;
 import java.util.Scanner;
 
 import ExportKML.WriteKML;
-import Row.Single_Row;
+import Row.Single_Row_After_Merge;
 
 public class IDFilter {
 
 	String userID;
-	Single_Row Single_Row_Object;
+	Single_Row_After_Merge Single_Row_After_Merge_Object;
 	WriteKML WriteKML_Object;
-	
+
 	public void ID(String userDestination, ArrayList<List<String>> Matrix){
 		System.out.println("Type the requested phone ID");
 		Scanner flag = new Scanner(System.in);
 		this.userID = flag.next();
 		IDfilter(Matrix, userID);
 		flag.close();
-		
+
 		WriteKML_Object  = new WriteKML();
 		WriteKML_Object.exportKml(userDestination, Matrix);
 	}
-	
+
 	private void IDfilter(ArrayList<List<String>> Matrix, String ID){
 		if(!isIDExist(Matrix, ID)) System.out.println("ID not exist!");
 		else{
 			for (int i = 1; i < Matrix.size(); i++) {
-				Single_Row_Object = new Single_Row(Matrix.get(i));
-				if(!Single_Row_Object.getID().equals(ID)){
+				Single_Row_After_Merge_Object = new Single_Row_After_Merge(Matrix.get(i));
+				if(!Single_Row_After_Merge_Object.getID().equals(ID)){
 					Matrix.remove(i);
 					i--;
 				}
@@ -38,8 +38,8 @@ public class IDFilter {
 	}
 	private boolean isIDExist(ArrayList<List<String>> Matrix, String ID){
 		for (int i = 1; i < Matrix.size(); i++) {
-			Single_Row_Object = new Single_Row(Matrix.get(i));
-			if(Single_Row_Object.getID().equals(ID)) return true;
+			Single_Row_After_Merge_Object = new Single_Row_After_Merge(Matrix.get(i));
+			if(Single_Row_After_Merge_Object.getID().equals(ID)) return true;
 		}
 		return false;
 	}
