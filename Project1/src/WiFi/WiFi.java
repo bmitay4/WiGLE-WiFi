b108dbff3fdd989c98dae4f0b502fbf20357f4a4
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-17 23:13:18 +0200

    Reverse for previous version

--- Project1/src/WiFi/WiFi.java
+++ Project1/src/WiFi/WiFi.java
@@ -11,9 +11,6 @@ public class WiFi {
 	String MAC;
 	String Channel;
 	String Signal;
-	String Lon;
-	String Lat;
-	String Alt;
 	List<String> Single_Row;
 	Single_Row Single_Row_Object;
 	
@@ -26,38 +23,26 @@ public class WiFi {
 		ArrayList<String> Ans_Row = new ArrayList<>();
 		Ans_Row.add(getSSID());
 		Ans_Row.add(getMAC());
-		Ans_Row.add(getLat());
-		Ans_Row.add(getLon());
-		Ans_Row.add(getAlt());	
 		Ans_Row.add(getChannel());
 		Ans_Row.add(getSignal());
 
 		return Ans_Row;
 	}
 	
-	public String getLat() {
-		return this.Single_Row.get(2);
-	}
 
-	public String getLon() {
-		return this.Single_Row.get(3);
-	}
-	public String getAlt() {
-		return this.Single_Row.get(4);
-	}	
 	public String getSSID() {
-		return this.Single_Row.get(6);
+		return Single_Row.get(6);
 	}
 
 	public String getMAC() {
-		return this.Single_Row.get(7);
+		return Single_Row.get(7);
 	}
 
 	public String getChannel() {
-		return this.Single_Row.get(8);
+		return Single_Row.get(8);
 	}
 
 	public String getSignal() {
-		return this.Single_Row.get(9);
+		return Single_Row.get(9);
 	}
 }
