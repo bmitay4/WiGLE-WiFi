af3adcf41600bb34d106e1473fdb494998b0941f
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-17 20:36:14 +0200

    Extends_2_A Updated

--- Project1/src/Extends_2/Find_AP_Location.java
+++ Project1/src/Extends_2/Find_AP_Location.java
@@ -8,11 +8,15 @@ import File.OpenFile;
 public class Find_AP_Location {
 
 	String userLocation;
-	ArrayList<List<String>> Matrix;
+	ArrayList<List<String>> Matrix, AP_Matrix;
 	OpenFile OpenFile_Object = new OpenFile();
 	Look_For_MAC Look_For_MAC_Object = new Look_For_MAC();
 	
 	public Find_AP_Location(String userLocation, String MAC){
 		this.Matrix = OpenFile_Object.openFiles(userLocation);
+		
+		if(!Look_For_MAC_Object.isContainsMAC(this.Matrix, MAC)) 
+			System.out.println("MAC Address not found");
 	}
+
 }
--- Project1/src/Extends_2/Look_For_MAC.java
+++ Project1/src/Extends_2/Look_For_MAC.java
@@ -3,7 +3,6 @@ package Extends_2;
 import java.util.ArrayList;
 import java.util.List;
 
-import File.OpenFile;
 import Row.Single_Row_After_Merge;
 import WiFi.All_WiFi;
 
@@ -12,20 +11,20 @@ public class Look_For_MAC {
 	List<String> WiFi_Row;
 
 	All_WiFi All_WiFi_Object;
-	Single_Row_After_Merge Single_Row_After_Merge_Object;
-	OpenFile OpenFile_Object;
+	Single_Row_After_Merge Row_After_Merge_Obj;
 
 	public boolean isContainsMAC(ArrayList<List<String>> Matrix, String MAC){
-		for (int i = 0; i < Matrix.size(); i++) {
-			Single_Row_After_Merge_Object = new Single_Row_After_Merge(Matrix.get(i));
+		for (int i = 1; i < Matrix.size(); i++) {
+			Row_After_Merge_Obj = new Single_Row_After_Merge(Matrix.get(i));
 		
-			if(Single_Row_After_Merge_Object.getSSID().equals(MAC)) return true;
+			if(Row_After_Merge_Obj.getMAC().equals(MAC)) return true;
 			
-			else if(Single_Row_After_Merge_Object.getNum_Of_WiFi_As_Integer() > 1){
+			else if(Row_After_Merge_Obj.getNum_Of_WiFi_As_Integer() > 1){
 				WiFi_Row = All_WiFi_Object.All_WiFi_Row(Matrix.get(i));
 				if(WiFi_Row.contains(MAC)) return true;
 			}
 		}
+		System.out.println("MAC Address not found");
 		return false;
 	}
 }
