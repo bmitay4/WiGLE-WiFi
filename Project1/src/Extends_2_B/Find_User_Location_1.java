306a9ded151fdac07641659d073b9597d911fdfd
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-18 22:14:40 +0200

    Test

--- Project1/src/Extends_2_B/Find_User_Location_1.java
+++ Project1/src/Extends_2_B/Find_User_Location_1.java
@@ -6,7 +6,7 @@ public class Find_User_Location_1 {
 
 	public void Program(String userLocation, String MAC1, int Singal1, String MAC2, int Singal2, String MAC3, int Singal3){
 		this.userLocation = userLocation;
-		
+		System.out.println();
 	}
 	
 }
