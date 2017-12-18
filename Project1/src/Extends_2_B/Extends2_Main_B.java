43299a90848d78a1240be83274ddd4dd2fa2a1c1
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-19 00:51:20 +0200

    Update

--- Project1/src/Extends_2_B/Extends2_Main_B.java
+++ Project1/src/Extends_2_B/Extends2_Main_B.java
@@ -1,16 +1,19 @@
 package Extends_2_B;
 
+import java.util.ArrayList;
+
 public class Extends2_Main_B {
 
 	public static void main(String[] args) {
 
 		String userLocation = "Test";
 		
-		Find_User_Location_1 Go1 = new Find_User_Location_1();
 		Find_User_Location_2 Go2 = new Find_User_Location_2();
 		
+		ArrayList<String> MAC1 = new ArrayList<>();
+		ArrayList<String> MAC2 = new ArrayList<>();
+		ArrayList<String> MAC3 = new ArrayList<>();
 		
-		Go1.Program(userLocation, MAC1, Singal1, MAC2, Singal2, MAC3, Singal3);
 		Go2.Program(userLocation, MAC1, MAC2, MAC3);
 	}
 
