88f8415006bb90091d760ef28ed804913669b133
חני <@10.9.2.121>
2017-12-18 22:33:33 +0200

    Test

--- Project1/src/Extends_2_B/Extends2_Main_B.java
+++ Project1/src/Extends_2_B/Extends2_Main_B.java
@@ -9,7 +9,7 @@ public class Extends2_Main_B {
 		Find_User_Location_1 Go1 = new Find_User_Location_1();
 		Find_User_Location_2 Go2 = new Find_User_Location_2();
 		
-				
+		System.out.println();
 		Go1.Program(userLocation, MAC1, Singal1, MAC2, Singal2, MAC3, Singal3);
 		Go2.Program(userLocation, MAC1, MAC2, MAC3);
 	}
