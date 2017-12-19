c7efb18b5971f854e65049ab441e52c138823f41
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-19 13:14:39 +0200

    Fixed WiFi Algo B

--- Project1/src/WiFi/WiFi_Algorithm_B.java
+++ Project1/src/WiFi/WiFi_Algorithm_B.java
@@ -5,29 +5,44 @@ import java.util.List;
 
 public class WiFi_Algorithm_B {
 	
-	String Lat;
-	String Lon;
-	String Alt;
-	ArrayList<String> WiFi_Row_B;
+	int NumOfWiFi;
+
+	ArrayList<String> Ans_Row = new ArrayList<>();
 	List<String> WiFi_Row;
 	
-	public WiFi_Algorithm_B(List<String> WiFi_Row){
+	public WiFi_Algorithm_B(List<String> WiFi_Row){	//Constructor
 		this.WiFi_Row = WiFi_Row;
-		this.Lat = getLat();
-		this.Lon = getLon();
-		this.Alt = getAlt();
-		this.Alt = getAlt();
-		this.Alt = getAlt();
+		this.NumOfWiFi = getNumOfWiFi();
+	}
+	public ArrayList<String> WiFi_B_Row(List<String> WiFi_Row){ //For a row with 1 MAC
+		this.WiFi_Row = WiFi_Row;
+		Ans_Row.add(getMAC());
+		Ans_Row.add(getSignal());
+		return Ans_Row;
+	}
+	public ArrayList<String> WiFis_B_Row(List<String> WiFi_Row){ //For a row with multiplication MAC
+		this.WiFi_Row = WiFi_Row;
+		for (int i = 0; i < NumOfWiFi; i++) {
+			Ans_Row.add(getMultiMAC(7 + NumOfWiFi*4));
+			Ans_Row.add(getMultiSignal(9 + NumOfWiFi*4));
+		}
+		return Ans_Row;
 	}
 	
-	public String getLat(){
-		return this.WiFi_Row.get(2);
+	public int getNumOfWiFi(){
+		return Integer.parseInt(this.WiFi_Row.get(5));
 	}
-	public String getLon(){
-		return this.WiFi_Row.get(3);
+	public String getMAC(){
+		return this.WiFi_Row.get(7);
 	}
-	public String getAlt(){
-		return this.WiFi_Row.get(4);
+	public String getSignal(){
+		return this.WiFi_Row.get(9);
 	}
-
+	public String getMultiMAC(int index){
+		return this.WiFi_Row.get(index);
+	}
+	public String getMultiSignal(int index){
+		return this.WiFi_Row.get(index);
+	}
+	
 }
