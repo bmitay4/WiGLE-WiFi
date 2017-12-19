b9f43d10d12233726b24e212982c61d23b83ac53
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-19 13:14:06 +0200

    The class determines if the returned row has 1 or more AP within

---
+++ Project1/src/Extends_2_B/AP_Counts.java
@@ -0,0 +1,22 @@
+package Extends_2_B;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import WiFi.WiFi_Algorithm_B;
+
+/**
+ *	This class determines if the returned row has 1 or more AP within
+ */
+
+public class AP_Counts {
+	
+	WiFi_Algorithm_B WiFi_B_Obj;
+	ArrayList<String> AP_Row;
+	
+	public AP_Counts(List<String> MAC){
+		WiFi_B_Obj = new WiFi_Algorithm_B(MAC);
+		if(WiFi_B_Obj.getNumOfWiFi() == 1) this.AP_Row = WiFi_B_Obj.WiFi_B_Row(MAC);
+		else this.AP_Row = WiFi_B_Obj.WiFis_B_Row(MAC);
+	}
+}
