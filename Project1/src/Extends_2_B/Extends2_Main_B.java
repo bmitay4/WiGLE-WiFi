cedabc2979a6232dad52095fa29a977241f45662
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-19 12:06:28 +0200

    Update

--- Project1/src/Extends_2_B/Extends2_Main_B.java
+++ Project1/src/Extends_2_B/Extends2_Main_B.java
@@ -6,15 +6,13 @@ public class Extends2_Main_B {
 
 	public static void main(String[] args) {
 
-		String userLocation = "Test";
+		String userLocation = "C:/Users/איתי בן משה/git/ArielOOP/Project1/Source_Files/Out/mergeCSV.csv";
 		
 		Find_User_Location_2 Go2 = new Find_User_Location_2();
 		
-		ArrayList<String> MAC1 = new ArrayList<>();
-		ArrayList<String> MAC2 = new ArrayList<>();
-		ArrayList<String> MAC3 = new ArrayList<>();
+		ArrayList<String> MAC = new ArrayList<>();
 		
-		Go2.Program(userLocation, MAC1, MAC2, MAC3);
+		Go2.Program(userLocation, MAC);
 	}
 
 }
