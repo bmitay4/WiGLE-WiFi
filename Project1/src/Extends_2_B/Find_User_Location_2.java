9030a49613186bd60d6a836b17e6f6fffcb68f29
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-24 22:37:38 +0200

    AlgoB file update

--- Project1/src/Extends_2_B/Find_User_Location_2.java
+++ Project1/src/Extends_2_B/Find_User_Location_2.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import File.OpenFile;
+import File.WriteFile;
 
 public class Find_User_Location_2 {
 
-	private ArrayList<List<String>> missingFileMatrix, completeFileMatrix;
+	private ArrayList<List<String>> missingFileMatrix, completeFileMatrix, FinalMatrix;
 
-	private OpenFile OpenFile_Obj1 = new OpenFile();
-	private OpenFile OpenFile_Obj2 = new OpenFile();
+	private OpenFile OpenFile_Obj = new OpenFile();
+	private WriteFile WriteFile_Obj = new WriteFile();
+	private Algorithm_B AlgoB = new Algorithm_B();
+	public void Program(String missingFile, String completeFile, int numOfSamples){
 
-	public void Program(String missingFile, String completeFile){
-
-		missingFileMatrix = OpenFile_Obj1.openFiles(missingFile);
-		completeFileMatrix = OpenFile_Obj2.openFiles(completeFile);
-
-		Algorithm_B AlgoB = new Algorithm_B(missingFileMatrix, completeFileMatrix);
+		this.missingFileMatrix = OpenFile_Obj.openFiles(missingFile);	//Generates matrix of the missing file
+		this.completeFileMatrix = OpenFile_Obj.openFiles(completeFile);	//Generates matrix of the complete file
+		this.FinalMatrix = AlgoB.Algorithm_B(missingFileMatrix, completeFileMatrix, numOfSamples);	//Generate the full and final matrix
+		
+		WriteFile_Obj.writeFiles(missingFile+" - Out.csv", FinalMatrix);	//Writing the full matrix to disk
 
 	}
 
