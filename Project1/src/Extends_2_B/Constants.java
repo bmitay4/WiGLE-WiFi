3bd559c339966c8932d6120de4532d0f2978eeee
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-24 22:38:07 +0200

    Constants class updated

---
+++ Project1/src/Extends_2_B/Constants.java
@@ -0,0 +1,24 @@
+package Extends_2_B;
+
+public class Constants {
+
+	private final int power = 2;
+	private final int normal = 10000;
+	private final double signal_diff = 0.4;
+	private final int min_diff = 3;
+	private final String no_signal = "-120";
+	private final int no_signal_diff = 100;
+
+	public int diff(String MacSignal, String Signal){
+		if(Signal.equals(no_signal)) return no_signal_diff;
+		else return Math.max(Math.abs(Integer.parseInt(MacSignal) - Integer.parseInt(Signal)), min_diff);
+	}
+
+	public double weight(String MacSignal, String diff){
+		int diffInt = Math.abs(Integer.parseInt(diff));
+		int macInt = Math.abs(Integer.parseInt(MacSignal));
+
+		return (normal/(Math.pow(diffInt, signal_diff)*Math.pow(macInt, power)));
+	}
+
+}
