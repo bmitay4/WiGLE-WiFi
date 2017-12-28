b9ff8fbb053d772cd07b4c491df3e3ab1fa261bd
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-28 12:00:35 +0200

    Upgrade

--- Project1/src/Extends_2_B/CombineAlgoA.java
+++ Project1/src/Extends_2_B/CombineAlgoA.java
@@ -18,21 +18,32 @@ public class CombineAlgoA {	//Combine Algo1 in Algo2
 		this.TotalGDetails = new ArrayList<>();
 
 		resetAPMatrix();
-		
+
 		APSumDetalis AlgoA = new APSumDetalis(APMatrix);
-		
+
 		//Changing the geographic details
-		AP_Row.remove(2);
-		AP_Row.remove(2);
-		AP_Row.remove(2);
-		AP_Row.add(2, String.valueOf(AlgoA.getAlt()));
-		AP_Row.add(2, String.valueOf(AlgoA.getLon()));
-		AP_Row.add(2, String.valueOf(AlgoA.getLat()));
+		removeOldGD(AP_Row);	//Remove old GD
+		setNewGD(AP_Row, AlgoA);	//Set new GD
+		
 		this.TotalGDetails = AP_Row;
+
 	}
 	private void resetAPMatrix(){	//Set the samples value according to numOfSamples input
 		for (int i = this.APMatrix.size() - 1; i >= this.numOfSamples; i--) {
 			this.APMatrix.remove(i);
+		}
+	}
+	private void removeOldGD(List<String> APRow){
+		APRow.remove(0);	//Remove date and time
+		for (int i = 0; i < 3; i++) {
+			APRow.remove(1);
+		}
+	}
+	private void setNewGD(List<String> APRow, APSumDetalis AlgoA){
+		for (int i = 0; i < 3; i++) {
+			APRow.add(1, String.valueOf(AlgoA.getAlt()));
+			APRow.add(1, String.valueOf(AlgoA.getLon()));
+			APRow.add(1, String.valueOf(AlgoA.getLat()));
 		}
 	}
 }
