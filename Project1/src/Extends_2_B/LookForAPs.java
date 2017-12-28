105540da9f4df9a13afbff420150469294e80a4a
Itay <33125525+itaybm@users.noreply.github.com>
2017-12-28 03:11:29 +0200

    Code upgrade

--- Project1/src/Extends_2_B/LookForAPs.java
+++ Project1/src/Extends_2_B/LookForAPs.java
@@ -5,21 +5,23 @@ import java.util.List;
 
 public class LookForAPs {
 
+	private ArrayList<List<String>> completeMatrix;
 	private RowToAP RowToAPObj = new RowToAP();
 	PiTools PiToolsObj = new PiTools();
 
 	public void setAPMatrix(ArrayList<List<String>> APMatrix, List<String> AP, ArrayList<List<String>> completeMatrix){ //If contain, add the row to the matrix		
+		this.completeMatrix = completeMatrix;
 		for (int i = 0; i < completeMatrix.size(); i++) {
-			if(isContain(AP, completeMatrix.get(i))){
+			if(isContain(AP, i)){
 				APMatrix.add(RowToAPObj.RowToAPrray(AP, completeMatrix.get(i)));
 				PiToolsObj.sortPi2(APMatrix);
 			}
 		}
 	}
 
-	private boolean isContain(List<String> AP, List<String> Row){ //Checks if a row contain one of the input mac
-		for (int i = 7; i < AP.size(); i = i + 4) {
-			if(Row.contains((AP.get(i)))) 
+	private boolean isContain(List<String> AP, int i){ //Checks if a row contain one of the input mac
+		for (int j = 7; j < AP.size(); j = j + 4) {
+			if(this.completeMatrix.get(i).contains((AP.get(j)))) 
 				return true;
 		}
 		return false;
