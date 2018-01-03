fdc7277817bdbf7ac9efd85277d6d0582f5005dc
avital477 <avital477@gmail.com>
2018-01-03 12:39:01 +0200

    GUIexsample

---
+++ Project1/src/testGUI.java
@@ -0,0 +1,63 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+
+public class testGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testGUI window = new testGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public testGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 21));
+		frame.getContentPane().setBackground(new Color(0, 0, 153));
+		frame.getContentPane().setForeground(new Color(0, 0, 102));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("example");
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "good choice:)");
+			}
+		});
+		btnNewButton.setBackground(new Color(102, 0, 102));
+		btnNewButton.setFont(new Font("Sylfaen", Font.BOLD, 26));
+		btnNewButton.setBounds(141, 107, 154, 68);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
