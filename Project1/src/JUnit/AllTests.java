93a5088bd61d0cf154c93cd6e3cd6bc43f76b8ee
Itay <33125525+itaybm@users.noreply.github.com>
2018-01-10 16:28:06 +0200

    Junit added

---
+++ Project1/src/JUnit/AllTests.java
@@ -0,0 +1,19 @@
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AllTests {
+
+	@Test
+	public void OpenFile() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void OpenFolder() {
+		fail("Not yet implemented");
+	}
+
+}
